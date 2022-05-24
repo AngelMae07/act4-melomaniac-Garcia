8b0b7caeda55a39eab9b99ad79a9c9b48684bc64
GarciaAngelMae <eyngelgarcia0@gmail.com>
2022-05-24 17:27:24 +0800

    Song1 Added

---
+++ song1.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+        body {
+                 background-color: darkblue;
+            }
+        </style>
+
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-2">
+                <a class="navbar-brand" href="#page-top">Musify</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                       <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song_5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Sun and Moon</h1>
+                <p class="lead">By Anees</p>
+                
+            </div>
+        
+
+        </header>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-7 text-center text-white">
+                      
+                    
+                        <?php
+                        //lyrics
+                        echo "<h2 class='text-white'>LYRICS</h2>";  
+                        echo "<p class='small'>[Intro]</p>";
+                        echo "<p>[CM7]  [B7]  [Em]  [Dm7]  (2x) </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus ]</p>";
+                        echo "<p> Baby, baby,[G7] you're my sun[CM7] and moon </p>";
+                        echo "<p> Girl, you're everything [Bm] between </p>";
+                        echo "<p>A lot of pretty [Em] faces could waste my time</p>";
+                        echo "<p> But you're [Dm7] my dream girl [G7]</p>";
+                        echo "<p> You make flowers [CM7] bloom </p>";
+                        echo "<p> Girl, you make the stars [B7] collide </p>";
+                        echo "<p> And I don't know [Em] what I did to get lucky like this </p>";
+                        echo "<p> But it sure feels fine </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Verse 1]</p>";
+                        echo "<p> [Dm7] ..'Cause you give [G7] me love [CM7]and affection</p>";
+                        echo "<p> I give you all [Bm] my protection</p>";
+                        echo "<p> And I'm tryna stay [Em] by your side 'til the day I die </p>";
+                        echo "<p> [Dm7] ..And you give [G7] me all [CM7] of your loving </p>";
+                        echo "<p> And I give you all [B7] of my trust </p>"; 
+                        echo "<p> I know you'll never [Em] betray me or leave my side, oh my </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus ]</p>";
+                        echo "<p> Baby, baby,[G7] you're my sun[CM7] and moon </p>";
+                        echo "<p> Girl, you're everything [Bm] between </p>";
+                        echo "<p>A lot of pretty [Em] faces could waste my time</p>";
+                        echo "<p> But you're [Dm7] my dream girl [G7]</p>";
+                        echo "<p> You make flowers [CM7] bloom </p>";
+                        echo "<p> Girl, you make the stars [B7] collide </p>";
+                        echo "<p> And I don't know [Em] what I did to get lucky like this </p>";
+                        echo "<p> But it sure feels fine </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Verse 2]</p>";
+                        echo "<p>[Dm7] ..’Cause I see [G7] your face [CM7] when I'm sleeping</p>";
+                        echo "<p> I wake up grateful [Bm] to breathe in</p>";
+                        echo "<p> The air you float [Em] through</p>";
+                        echo "<p> I wrote you a lullaby </p>";
+                        echo "<p> [Dm7] A song [G7] to sing CM7]on your pillow</p>";
+                        echo "<p> Swift as a swing [B7] on a willow</p>";
+                        echo "<p> My sweetest words [Em] are mere birds</p>";
+                        echo "<p> In your perfect sky, oh my [Dm7] </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus ]</p>";
+                        echo "<p> Baby, baby,[G7] you're my sun[CM7] and moon </p>";
+                        echo "<p> Girl, you're everything [Bm] between </p>";
+                        echo "<p>A lot of pretty [Em] faces could waste my time</p>";
+                        echo "<p> But you're [Dm7] my dream girl [G7]</p>";
+                        echo "<p> You make flowers [CM7] bloom </p>";
+                        echo "<p> Girl, you make the stars [B7] collide </p>";
+                        echo "<p> And I don't know [Em] what I did to get lucky like this </p>";
+                        echo "<p> But it sure feels fine </p>";
+                        echo  "<p> (2x) </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Outro]</p>";
+                        echo "<p> [CM7]…Oh …my[Bm] </p>";
+                        echo "<p> Oh my, oh my, [Em] </p>";
+                        echo "<p>  Yeah Oh my, oh my [Dm7] </p>";
+                        echo "<p> Baby, baby, [G7]you're mine </p>";
+                        echo "<br/>";
+
+                       
+                        ?>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Angel Mae Garcia</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
